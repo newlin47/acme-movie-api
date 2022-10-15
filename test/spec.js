e3000ba63ed6e74f4a0875e9b1f5c0ce363eb1c1
Ethan Newlin <newlin5@gmail.com>
2022-10-15 11:33:07 -0400

    supertest working

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,17 @@
+const app = require('supertest')(require('../app'));
 const { expect } = require('chai');
-const two = require('../index.js');
+const two = require('../index');
 
 describe('Our App', () => {
-	it('equals two', () => {
-		expect(two).to.equal(2);
+	describe('app basics', () => {
+		it('equals two', () => {
+			expect(two).to.equal(2);
+		});
+	});
+	describe('homepage', () => {
+		it('has sample html', async () => {
+			const response = await app.get('/');
+			expect(response.status).to.equal(200);
+		});
 	});
 });
