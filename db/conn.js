0cd29a9bf5871d31cd545872eef98b5b632d4ac4
Ethan Newlin <newlin5@gmail.com>
2022-10-15 11:54:15 -0400

    sync and seed

--- db/conn.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-
-const conn = new Sequelize(
-	process.env.DATABASE_URL || 'postgres://localhost/acme_movies_db'
-);
-
-module.exports = conn;
--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,15 @@
 const app = require('supertest')(require('../app'));
 const { expect } = require('chai');
+const {
+	syncAndSeed,
+	models: { Movie },
+} = require('../db');
 const two = require('../index');
 
 describe('Our App', () => {
+	beforeEach(async () => {
+		await syncAndSeed();
+	});
 	describe('app basics', () => {
 		it('equals two', () => {
 			expect(two).to.equal(2);
@@ -12,6 +19,12 @@ describe('Our App', () => {
 		it('has sample html', async () => {
 			const response = await app.get('/');
 			expect(response.status).to.equal(200);
+		});
+	});
+	describe('movies', () => {
+		it('has four movies', async () => {
+			const movies = await Movie.findAll();
+			expect(movies.length).to.equal(4);
 		});
 	});
 });
